f6012043dac62ff8896dfc5c5e12923b1b577107
WEB Contest <contest@kitm.local>
2025-02-27 09:53:12 +0200

    template

---
+++ firebase.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getFirebase } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDNwctHmC4EPPT_WcIRkwGH3uZaT6b86ak",
+  authDomain: "webchallenge-151c2.firebaseapp.com",
+  projectId: "webchallenge-151c2",
+  storageBucket: "webchallenge-151c2.firebasestorage.app",
+  messagingSenderId: "660022860523",
+  appId: "1:660022860523:web:78f025963122e1eab5fcae",
+  measurementId: "G-1398V97HQM"
+
+};
+
+
+
+export const app = initializeApp(firebaseConfig);
+
+
