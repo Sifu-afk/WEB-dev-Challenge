cc77c30575865f36acf967e0e91211dae36507e0
WEB Contest <contest@kitm.local>
2025-02-27 15:34:29 +0200

    all the work

--- app.js
+++ app.js
@@ -5,19 +5,33 @@ import {  getAuth  } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-au
 const db = getDatabase(app)
 
 
-const inputVin = document.getElementById('vin')
+const inputName = document.getElementById('names')
+const inputLast = document.getElementById('last')
+const inputEmail = document.getElementById('email')
+const inputModel= document.getElementById('model')
+const inputJob = document.getElementById('job')
+const inputproblem = document.getElementById('problem')
+const inputDate = document.getElementById('date')
+
+
 const submit = document.getElementById('submit')
 
-// submit.addEventListener('click', () =>{
-//     event.preventDefault()
+submit.addEventListener('click', () =>{
+    event.preventDefault()
         
-//     set(ref(db, 'car/' + vin.value), {
-//         Vin: vin.value
-//     })
-//         .then(() => {
-//             alert('data added')
-//         })
-//         .catch((error) => {
-//             alert(error)
-//         })
-//     })
+    set(ref(db, 'customers/' + names.value), {
+        Name: names.value,
+        LastName: last.value,
+        Email: email.value,
+        CarModel: model.value,
+        ServiceTye: job.value,
+        DetailedProblem: problem.value,
+        DateForRepair: inputDate.value
+    })
+        .then(() => {
+            alert('data added')
+        })
+        .catch((error) => {
+            alert(error)
+        })
+    })
